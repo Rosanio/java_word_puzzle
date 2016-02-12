16ef47aa28e592d742c28998a0bb9a0fa633c63e
Matt Rosanio <mmrosani@syr.edu>
2016-02-12 08:11:07 -0800

    Create failing test to remove all vowels

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -10,4 +10,10 @@ public class AppTest {
     App testApp = new App();
     assertEquals("-pple", testApp.removeVowels("apple"));
   }
+
+  @Test
+  public void removeVowels_replaceVowels_returnStringWithNoVowels() {
+    App testApp = new App();
+    assertEquals("th-s c-d- r-v--w -s --sy", testApp.removeVowels("this code review is easy"));
+  }
 }
