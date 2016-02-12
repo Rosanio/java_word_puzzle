9dd0803be8b0700082112fbf6a520f9b0ffbd94c
Matt Rosanio <mmrosani@syr.edu>
2016-02-12 08:00:15 -0800

    Create failing test to remove letter 'a'

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,5 +4,10 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 
 public class AppTest {
-  
+
+  @Test
+  public void removeVowels_removeLetterA_returnStringWithNoA() {
+    App testApp = new App();
+    assertEquals("-pple", testApp.removeVowels("apple"));
+  }
 }
